e9b6c8455f820852e449caa06595a4df234ab958
Vamsi <bhagivamsi@gmail.com>
2022-08-03 23:47:44 -0400

    CORS

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const initiateMongoServer = require("./config/db");
-
+const cors = require("cors");
 var app = express();
 
 // view engine setup
@@ -12,6 +12,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
