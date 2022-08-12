116d5de6278ae5d8d88854a3aecf7cbd4b31aabf
Vamsi <bhagivamsi@gmail.com>
2022-08-12 00:03:53 -0400

    Carousel onClick

--- frontend/src/homepage/HomepageCarousel.js
+++ frontend/src/homepage/HomepageCarousel.js
@@ -2,27 +2,37 @@ import React, { useEffect, useState } from "react";
 import { Carousel } from "react-bootstrap";
 import { BACKEND_BASE_IMAGE_URL } from "../Common/Config";
 import { getCarouselData } from "./HomepageService";
+import { useNavigate } from "react-router-dom";
 
 export function HomepageCarousel() {
   let [carouselData, updateCarouselData] = useState([]);
   useEffect(() => {
     getCarouselData(updateCarouselData);
   }, []);
+
+  const navigate = useNavigate();
+
   return (
     <Carousel
-      autoPlay={false}
-      interval={null}
+      autoPlay={true}
+      interval={1800}
       controls={false}
       indicators={true}
       variant="dark"
     >
       {carouselData.map((item) => (
-        <Carousel.Item key={item._id}>
+        <Carousel.Item
+          key={item._id}
+          role="button"
+          onClick={() =>
+            navigate("/products/" + item._id, { replace: false }, [navigate])
+          }
+        >
           <img
             className="rounded mx-auto d-block"
             src={BACKEND_BASE_IMAGE_URL + item.image}
             alt=""
-            style={{ objectFit: "cover" }}
+            style={{ height: "300px", width: "100%", objectFit: "cover" }}
           />
           <Carousel.Caption
             // style={{ position: "relative" }}
